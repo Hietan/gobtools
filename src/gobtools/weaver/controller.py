94ddc50b82754eaf6d5b36cb3683aef242f363a9
Hietan <hietan622@gmail.com>
2024-11-05 08:53:05 +0900

    add cypher and artifact

--- src/gobtools/weaver/controller.py
+++ src/gobtools/weaver/controller.py
@@ -22,3 +22,26 @@ class WeaverController(Client):
     
     def get_dependents(self, group_id: str, artifact_id: str, version: str, added_values: list[str] = None) -> dict:
         return self.__post_releases("/release/dependents", group_id, artifact_id, version, added_values)
+
+    def get_cypher(self, query: str, added_values: list[str] = None) -> dict:
+        data = {
+            "query": query
+        }
+        if added_values:
+            data["addedValues"] = added_values
+        return self.post_dict("/cypher", data)
+    
+    def __post_artifact(self, path: str, group_id: str, artifact_id: str, added_values: list[str] = None) -> dict:
+        data = {
+            "groupId": group_id,
+            "artifactId": artifact_id
+        }
+        if added_values:
+            data["addedValues"] = added_values
+        return self.post_dict(path, data)
+    
+    def get_artifact(self, group_id: str, artifact_id: str, added_values: list[str] = None) -> dict:
+        return self.__post_artifact(group_id, artifact_id, added_values)
+    
+    def get_artifact_releases(self, group_id: str, artifact_id: str, added_values: list[str] = None) -> dict:
+        return self.__post_artifact("/artifact/releases", group_id, artifact_id, added_values)
